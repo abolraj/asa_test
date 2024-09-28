908034c3a1d4649db58fa954369fe9a2b0b22349
abolraj <abolfazlrajaeenasab@gmail.com>
2024-09-28 09:54:16 +0330

    fix - Fix Many to Many realtionship

--- orders_db.sql
+++ orders_db.sql
@@ -17,11 +17,19 @@ CREATE table products (
 /* orders table */
 CREATE table orders (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    product_id INT NOT NULL,
     user_id INT NOT NULL,
     status enum('ordered','delivering','paid','delivered','cancelled'),
     ordered_at TIMESTAMP NOT NULL
 );
+
+/* orders products table */
+CREATE table orders_products (
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    FOREGIN KEY(order_id) REFERENCES orders(id),
+    FOREGIN KEY(product_id) REFERENCES product(id),
+);
+
 
 INSERT INTO products (`id`, `name`,`price`,`available_no`) VALUES 
 (1, 'airpod', 12000, 10),
@@ -32,24 +40,24 @@ INSERT INTO products (`id`, `name`,`price`,`available_no`) VALUES
 (6, 'banana', 14000, 5),
 (7, 'tomato', 156000, 6);
 
-INSERT INTO orders (`id`, `product_id`,`user_id`,`status`, `ordered_at`) VALUES
-(1, 1, 1, 'ordered', '2020-10-21 12:30:23'),
-(1, 1, 2, 'ordered', '2020-10-21 12:30:23'),
-(1, 3, 2, 'ordered', '2020-10-21 12:30:23'),
-(1, 3, 3, 'delivered', '2020-10-21 12:30:23'),
-(1, 3, 1, 'paid', null),
-(1, 4, 1, 'delivering', '2020-10-21 12:30:23'),
-(1, 4, 1, 'delivering', '2020-10-21 12:30:23'),
-(1, 4, 3, 'delivering', null),
-(1, 4, 1, 'paid', '2020-10-21 12:30:23'),
-(1, 4, 1, 'paid', '2020-10-21 12:30:23'),
-(1, 4, 1, 'delivered', '2020-10-21 12:30:23'),
-(1, 5, 1, 'delivered', null),
-(1, 5, 1, 'delivered', '2020-10-21 12:30:23'),
-(1, 5, 3, 'delivered', '2020-10-21 12:30:23'),
-(1, 5, 1, 'cancelled', '2020-10-21 12:30:23'),
-(1, 6, 3, 'cancelled', '2020-10-21 12:30:23'),
-(1, 6, 1, 'cancelled', '2020-10-21 12:30:23'),
-(1, 6, 3, 'cancelled', '2020-10-21 12:30:23'),
-(1, 6, 4, 'cancelled', '2020-10-21 12:30:23'),
-(1, 6, 1, 'cancelled', '2020-10-21 12:30:23');
+INSERT INTO orders (`id`,`status`, `ordered_at`) VALUES
+(1 'ordered', '2020-10-21 12:30:23'),
+(2, 'ordered', '2020-10-21 12:30:23'),
+(3, 'ordered', '2020-10-21 12:30:23'),
+(4, 'delivered', '2020-10-21 12:30:23'),
+(5, 'paid', null),
+(6, 'delivering', '2020-10-21 12:30:23'),
+(7, 'delivering', '2020-10-21 12:30:23'),
+(8, 'delivering', null),
+(9, 'paid', '2020-10-21 12:30:23'),
+(10, 'paid', '2020-10-21 12:30:23'),
+(11, 'delivered', '2020-10-21 12:30:23'),
+(12, 'delivered', null),
+(13, 'delivered', '2020-10-21 12:30:23'),
+(14, 'delivered', '2020-10-21 12:30:23'),
+(15, 'cancelled', '2020-10-21 12:30:23'),
+(16, 'cancelled', '2020-10-21 12:30:23'),
+(17, 'cancelled', '2020-10-21 12:30:23'),
+(18, 'cancelled', '2020-10-21 12:30:23'),
+(19, 'cancelled', '2020-10-21 12:30:23'),
+(20, 'cancelled', '2020-10-21 12:30:23');
